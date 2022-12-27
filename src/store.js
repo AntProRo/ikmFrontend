3ffebf6434f24624f176b5c7a2fc9c60cf4d5cdc
Antonio Rodriguez <anrodriguez@apexsystems.com>
2022-12-27 08:46:30 -0600

    first commit

--- src/store.js
+++ src/store.js
@@ -2,7 +2,6 @@ import { legacy_createStore, applyMiddleware} from 'redux';
 import {  composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
-
 const initialState = {
 
 }
